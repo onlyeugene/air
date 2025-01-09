34212b383a6c84ce4d4525f50c4b0a993fb4ffc4
Eugene <talktoeugenedavid@icloud.com>
2025-01-09 08:09:42 +0100

    new update

--- app/components/empy-state.tsx
+++ app/components/empy-state.tsx
@@ -11,7 +11,7 @@ interface EmptyStateProps {
 }
 
 const EmptyState:React.FC<EmptyStateProps> = ({
-    title = "No exact matche",
+    title = "No exact matches",
     subtitle = "Try changing or removing some of your filters",
     showReset
 }) => {
